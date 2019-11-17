801ed19c65ec48e51195b7b0f5dce137514315cb
Dom <domeccleston@gmail.com>
2019-11-17 23:11:59 +0000

    seeded db and added register route

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,11 +1,25 @@
-const authRouter = require('express').Router();
+const authRouter = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require('./authModel')
 
-authRouter.post('/login', () => {
+authRouter.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 11);
 
-})
+  const newUser = {
+    username: req.body.username,
+    password: hash
+  };
 
-authRouter.post('/register', () => {
+  Users.addUser(newUser)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-})
+authRouter.post("/login", () => {});
 
 module.exports = authRouter;
