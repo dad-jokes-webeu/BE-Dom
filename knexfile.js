fc0432f9b26f056be453708d002310194c503d81
Dom <domeccleston@gmail.com>
2019-11-17 22:36:06 +0000

    add users migration

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
     useNullAsDefault: true,
   },
 
