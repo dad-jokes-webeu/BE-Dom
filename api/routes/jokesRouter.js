fc397e6ce9596b931e0ea34ad1642647afc38a2d
Dom <domeccleston@gmail.com>
2019-11-19 16:22:27 +0000

    restricted routes

--- api/routes/jokesRouter.js
+++ api/routes/jokesRouter.js
@@ -1,27 +1,28 @@
 const jokesRouter = require('express').Router();
 const Jokes = require('../models/jokesModel')
+const { restricted } = require('../../middleware/index');
 
-jokesRouter.get('/', async (req, res) => {
+jokesRouter.get('/', restricted, async (req, res) => {
     const jokes = await Jokes.getJokes();
     res.json(jokes);
 })
 
-jokesRouter.get('/:id', async(req, res) => {
+jokesRouter.get('/:id', restricted, async(req, res) => {
     const joke = await Jokes.getJokeById(req.params.id);
     res.json(joke);
 })
 
-jokesRouter.post('/', async (req, res) => {
+jokesRouter.post('/', restricted, async (req, res) => {
     const newJoke = await Jokes.addJoke(req.body);
     res.json(newJoke);
 })
 
-jokesRouter.put('/:id', async (req, res) => {
+jokesRouter.put('/:id', restricted, async (req, res) => {
     const updatedJoke = await Jokes.updateJoke(req.params.id, req.body);
     res.json(updatedJoke);
 })
 
-jokesRouter.delete('/:id', async (req, res) => {
+jokesRouter.delete('/:id', restricted, async (req, res) => {
     const deletedJoke = await Jokes.deleteJoke(req.params.id, req.body)
     res.json({message: "Deleted the following joke:", joke: deletedJoke })
 })
