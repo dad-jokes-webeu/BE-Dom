b4bf731f74753891c12d4db7f7fe106bb6dfd0a4
Dom <domeccleston@gmail.com>
2019-11-19 12:55:50 +0000

    jokes post endp

--- api/models/jokesModel.js
+++ api/models/jokesModel.js
@@ -1,9 +1,19 @@
 const db = require('../../data/dbConfig');
 
 function getJokes() {
-    return db('jokes').orderBy();
+    return db('jokes')
+}
+
+function getJokeById(id) {
+    return db('jokes').where({ id }).first();
+}
+
+function addJoke(joke) {
+    return db('jokes').insert(joke);
 }
 
 module.exports = {
     getJokes,
+    getJokeById,
+    addJoke,
 }
--- api/routes/jokesRouter.js
+++ api/routes/jokesRouter.js
@@ -6,4 +6,9 @@ jokesRouter.get('/', async (req, res) => {
     res.json(jokes);
 })
 
+jokesRouter.post('/', async (req, res) => {
+    const newJoke = await Jokes.addJoke(req.body);
+    res.json(newJoke);
+})
+
 module.exports = jokesRouter;
