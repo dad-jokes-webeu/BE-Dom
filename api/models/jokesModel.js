7e8049d96c222a78531e97b7d9c2f7f3f045ac55
Dom <domeccleston@gmail.com>
2019-11-19 13:27:37 +0000

    further crud endpoints

--- api/models/jokesModel.js
+++ api/models/jokesModel.js
@@ -1,19 +1,37 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 function getJokes() {
-    return db('jokes')
+  return db("jokes");
 }
 
 function getJokeById(id) {
-    return db('jokes').where({ id }).first();
+  return db("jokes")
+    .where({ id })
+    .first();
 }
 
 function addJoke(joke) {
-    return db('jokes').insert(joke);
+  return db("jokes").insert(joke);
+}
+
+function updateJoke(id, joke) {
+  return db("jokes")
+    .where({ id })
+    .update(joke);
+}
+
+function deleteJoke(id, joke) {
+  const deletedJoke = getJokeById(id);
+  db("jokes")
+    .where({ id })
+    .del();
+  return deletedJoke;
 }
 
 module.exports = {
-    getJokes,
-    getJokeById,
-    addJoke,
-}
+  getJokes,
+  getJokeById,
+  addJoke,
+  updateJoke,
+  deleteJoke,
+};
--- api/routes/jokesRouter.js
+++ api/routes/jokesRouter.js
@@ -2,13 +2,28 @@ const jokesRouter = require('express').Router();
 const Jokes = require('../models/jokesModel')
 
 jokesRouter.get('/', async (req, res) => {
-    const jokes = await Jokes.getJokes()
+    const jokes = await Jokes.getJokes();
     res.json(jokes);
+})
+
+jokesRouter.get('/:id', async(req, res) => {
+    const joke = await Jokes.getJokeById(req.params.id);
+    res.json(joke);
 })
 
 jokesRouter.post('/', async (req, res) => {
     const newJoke = await Jokes.addJoke(req.body);
     res.json(newJoke);
+})
+
+jokesRouter.put('/:id', async (req, res) => {
+    const updatedJoke = await Jokes.updateJoke(req.params.id, req.body);
+    res.json(updatedJoke);
+})
+
+jokesRouter.delete('/:id', async (req, res) => {
+    const deletedJoke = await Jokes.deleteJoke(req.params.id, req.body)
+    res.json({message: "Deleted the following joke:", joke: deletedJoke })
 })
 
 module.exports = jokesRouter;
